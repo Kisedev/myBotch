665e1108038ace7005645a26169cd0cedd01d936
Kise <ruosena@gmail.com>
2019-04-28 22:04:07 -0300

    log and response on bot mention

--- app.js
+++ app.js
@@ -1,7 +1,34 @@
-const Discord = require('discord.js')
-const client = new Discord.Client()
+const Discord = require('discord.js');
+const client = new Discord.Client();
+let myBotchName
 
+//Bot start
 client.login('NTcxODQ2NDYzMDcyNTAxNzYw.XMTwLg.nYVJCCozlFNrKeAVIqccgJSs5PY')
 client.on('ready', () => {
+    myBotchName = client.user.tag.slice(0, client.user.tag.indexOf("#"));
     console.log(`${client.user.tag} says: I'm ready to serve Senpai`)
+})
+
+// - Core -
+// Dialogues
+// *Criar metodo para pegar uma fala random*
+const speeches = {
+    greetings: ["Hello!"]
+}
+
+// Functions
+function sayMyName(msg) {
+    return msg.startsWith(`!${myBotchName}`)
+}
+
+client.on('message', async msg => {
+    // Me chamou senpai?
+    if (sayMyName(msg.content)) {
+        speech = msg.content.slice(myBotchName.length + 1).trim()
+        console.log(`-${speech}`)
+
+        if (speech == "hi") {
+            msg.reply(speeches.greetings[0])
+        }
+    }
 })
